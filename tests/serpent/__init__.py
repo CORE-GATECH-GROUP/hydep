addbcff57e1e4b2734ce48d60ee9745c9209ec31
Andrew Johnson <drewej@protonmail.com>
2019-12-30 23:35:27 -0500

    Introduce some string / file comparison test helpers

---
+++ tests/serpent/__init__.py
@@ -0,0 +1,34 @@
+import filecmp
+import difflib
+
+
+def showStringDiff(reference, written, fromfile="reference", tofile="reference"):
+    diff = difflib.unified_diff(
+        reference.splitlines(keepends=True),
+        written.splitlines(keepends=True),
+        fromfile=fromfile,
+        tofile=tofile,
+    )
+    print("".join(diff))
+
+
+def strcompare(reference, written):
+    if reference == written:
+        return True
+    showStringDiff(reference, written)
+    return False
+
+
+def filecompare(rpath, apath, failpath):
+    if filecmp.cmp(rpath, apath):
+        return True
+
+    apath.rename(failpath)
+
+    showStringDiff(
+        rpath.read_text(),
+        failpath.read_text(),
+        fromfile=rpath.name,
+        tofile=failpath.name,
+    )
+    return False
