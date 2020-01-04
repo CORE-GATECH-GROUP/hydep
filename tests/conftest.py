c3120353e900ee967286929162b09b8341889859
Andrew Johnson <drewej@protonmail.com>
2020-01-04 10:17:11 -0500

    File-based depletion chain fixture in conftests
    Will be used in other unit tests. Now a file-based fixture
    as well, using newly tracked simple_chain.xml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 """Configuration options for pytest"""
+import pathlib
+
+import pytest
+from hydep import DepletionChain
 
 from tests.regressions import config as regressionCfg
+
 
 def pytest_addoption(parser):
     parser.addoption("--update", action="store_true")
@@ -10,3 +15,9 @@ def pytest_configure(config):
     upd = config.getoption("update")
     if upd is not None:
         regressionCfg["update"] = upd
+
+
+@pytest.fixture(scope="session")
+def simpleChain():
+    chainfile = pathlib.Path(__file__).parent / "simple_chain.xml"
+    yield DepletionChain.fromXml(str(chainfile))
