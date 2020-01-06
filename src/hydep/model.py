b1b15d633a86788ca9c64c671e6fa6e375a851d0
Andrew Johnson <ajohnson400@gatech.edu>
2020-01-06 13:14:04 -0500

    Update volumes with Model.differentiateBurnableMaterials
    An additional argument is given to the method, denoting if the
    user wants to update the volumes of burnable materials encountered.
    This triggers Universe.countBurnableMaterials to compute the
    number of times each material is encountered, and then scale the volume
    accordingly. A check is performed to ensure the volume is provided
    for each burnable material.

--- src/hydep/model.py
+++ src/hydep/model.py
@@ -16,18 +16,50 @@ class Model:
         self.root = root
         self._bounds = False  # Special value since None is valid
 
-    def differentiateBurnableMaterials(self):
+    def differentiateBurnableMaterials(self, updateVolumes=True):
         """Create new burnable materials across the geometry.
+
+        .. note::
+
+            Calling this method more than once without changes
+            to :attr:`model` is redundant. The model will be
+            udpated such that each burnable material occurs only
+            once after the  first call, and there is nothing more
+            to perform on the second call.
 
         This routine is important to create unique burnable materials
         that can be depleted using spatially correct fluxes and
         reaction rates.
 
         This method digs through contained universes and creates unique
-        :class:`hydep.BurnedMaterial` objects and potentially new
-        interior universes.
+        :class:`hydep.BurnableMaterial` objects and potentially new
+        interior universes. By default, the volume of each repeated
+        :class:`hydep.BurnableMaterial` will be scaled according to
+        the number of occurances found in the :attr:`model`. This
+        behavior can be controlled with the ``updateVolumes``
+        argument.
+
+        Parameters
+        ----------
+        updateVolumes : bool
+            If provided, material volumes will also be scaled by the
+            number of occurances before creating new materials.
+
+        Raises
+        ------
+        AttributeError
+            If ``updateVolumes`` is True and
+            :attr:`hydep.BurnableMaterial.volume` is not set
+            for a burnable material
 
         """
+        if updateVolumes:
+            vols = self.root.countBurnableMaterials()
+            for mat, counts in vols.values():
+                if mat.volume is None:
+                    raise AttributeError("Volume not set for {}".format(mat))
+                mat.volume = mat.volume / counts
+
         return self.root.differentiateBurnableMaterials()
 
     @property
