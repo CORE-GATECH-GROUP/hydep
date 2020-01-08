9861c1afb6ace2e51e5afbaf865b0cbae9a0cdbc
Andrew Johnson <drewej@protonmail.com>
2020-01-07 23:34:31 -0500

    Start SerpentSolver regression test - flux, keff
    Create a regression test for the Serpent solver that utilizes a
    2x2 lattice, but with different burnable materials in each region.
    The solver is configured to use a dedicated random seed, currently
    just 12345678910, to help ensure reproducible results.
    
    The Solver goes through the main aspects of the interface:
    1) beforeMain <- model and burnable materiasl
    2) bosUpdate <- dummy concentrations, time 0, nominal power
    3) execute
    4) processResults -> TransportResult
    
    The test relies on a temporary directory for the simulation, so the
    base file and potential zips are not added to the user's working
    directory. These may be helpful on failures, buuut that's for later.
    
    Comparisons are performed using the ResultComparator so that
    reference data is extracted in a "standardized" way. This also
    allows the test writer to not worry about creating failure files
    at the end.

---
+++ tests/regressions/serpent2x2/test_solver.py
@@ -0,0 +1,92 @@
+from collections import namedtuple
+import pathlib
+
+import pytest
+import hydep
+import hydep.internal
+import hydep.serpent
+import hydep.internal.features as hdfeat
+
+from tests.regressions import config, ResultComparator
+
+
+ProblemProxy = namedtuple("ProblemProxy", "model burnable")
+
+
+@pytest.fixture
+def serpentModel(simpleChain, toy2x2lattice):
+    # Include the chain so reactions are present
+    model = hydep.Model(toy2x2lattice)
+    model.differentiateBurnableMaterials(updateVolumes=False)
+
+    burnable = tuple(model.root.findBurnableMaterials())
+    for m in burnable:
+        m.volume = 1.0
+
+    yield ProblemProxy(model, burnable)
+
+
+@pytest.fixture
+def serpentSolver(tmpdir):
+    options = {
+        "hydep": {"archive on success": True},
+        "hydep.serpent": {
+            "random seed": 12345678910,
+            "boundary conditions": "reflective",
+            "particles": 100,
+            "generations per batch": 2,
+            "active": 5,
+            "skipped": 2,
+            "executable": "sss2",
+        },
+    }
+
+    solver = hydep.serpent.SerpentSolver()
+    solver.configure(options)
+
+    with tmpdir.as_cwd():
+        tmpdir.mkdir("serpent")
+        yield solver
+        solver.finalize(True)
+
+
+def test_serpentSolver(serpentSolver, serpentModel):
+    model = serpentModel.model
+    burnable = serpentModel.burnable
+
+    # Set hooks for slightly realistic problem
+    XS_KEYS = {"abs", "fiss"}
+    hooks = hdfeat.FeatureCollection({hdfeat.HOMOG_LOCAL}, XS_KEYS)
+
+    serpentSolver.setHooks(hooks)
+
+    assert serpentSolver.hooks == hooks
+
+    serpentSolver.beforeMain(model, burnable)
+
+    timeStep = hydep.internal.TimeStep(0, 0, 0, 0)
+
+    # Concentrations for the current step
+    # Not sure how these will look in the future
+    # Set to None to catch errors down the road
+
+    DUMMY_CONCS = None
+
+    # Set a realistic power for this time step
+
+    POWER = 6e6
+
+    serpentSolver.bosUpdate(DUMMY_CONCS, timeStep, POWER)
+
+    serpentSolver.execute()
+
+    res = serpentSolver.processResults()
+
+    tester = ResultComparator(pathlib.Path(__file__).parent)
+
+    if config.get("update"):
+        tester.update(res)
+        return
+
+    failures = tester.compare(res)
+    assert not failures
