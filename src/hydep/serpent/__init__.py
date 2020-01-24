3174a0b205ea37f78fdd91d1562feb6177ced8d6
Andrew Johnson <ajohnson400@gatech.edu>
2020-01-24 16:05:43 -0500

    Provide useful error if serpentTools not installed

--- src/hydep/serpent/__init__.py
+++ src/hydep/serpent/__init__.py
@@ -1,6 +1,12 @@
 """
 Serpent high fidelity transport solver interface
 """
+try:
+    import serpentTools
+except ImportError:
+    raise ImportError(
+        "serpentTools required for Serpent interface. Install as extra "
+        "with pip install <options> hydep[serpent]")
 
 from .writer import SerpentWriter
 from .runner import SerpentRunner
