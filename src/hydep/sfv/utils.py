508d0e3a556b67e559007a0d930d08812decf4ff
Andrew Johnson <drewej@protonmail.com>
2020-02-05 21:38:39 -0500

    Provide NubarPolyFit utility in sfv module
    SFV method needs a way to predict the new average number of
    neutrons produced per fission. Current practice is to extrapolate
    or interpolate based on previous time steps, where the nubar data
    are homogenized in each burnable region. This is essentially like
    a one-group "macroscopic" nubar number.
    
    The NubarPolyFit class fulfills the TimeTraveller interface for
    storing one-group nubar values in each burnable region. It is only
    intended to be used by the SFV interface, and is tucked inside. Users
    have control over number of previous points and fitting order,
    defaulting to a total of three points, and linear fitting.
    
    Unit tests have been added to explore the functionality of this class.

---
+++ src/hydep/sfv/utils.py
@@ -0,0 +1,109 @@
+import math
+import numbers
+from collections import deque
+
+import numpy
+from numpy.polynomial.polynomial import polyfit, polyval
+
+from hydep.internal.timetravel import TimeTraveller
+
+
+class NubarPolyFit(TimeTraveller):
+    """Class for performing polynomial extrapolation on nubar
+
+    .. note::
+
+        One group only, provided in each burnable material
+
+    Parameters
+    ----------
+    maxlen : int, optional
+        Maximum number of time points to retain
+    order : int, optional
+        Order for the polynomial fitting
+
+    Attributes
+    ----------
+    times : tuple of float
+        Copy of the underlying times used.
+    nubar : tuple of numpy.ndarray
+        Copy of stored nubar values
+    order : int
+        Read-only polynomial fitting order
+
+    """
+
+    __slots__ = TimeTraveller.__slots__ + ("_order", "_nubar", "_coeffs")
+
+    def __init__(self, maxlen=3, order=1):
+        assert isinstance(order, numbers.Integral) and order >= 0
+        if maxlen is not None:
+            assert isinstance(maxlen, numbers.Integral) and order < maxlen
+        super().__init__(maxlen=maxlen)
+        self._order = order
+        self._nubar = deque(maxlen=maxlen)
+        self._coeffs = None
+
+    @property
+    def order(self):
+        return self._order
+
+    @property
+    def nubar(self):
+        return tuple(self._nubar)
+
+    def _validate(self, item):
+        data = numpy.asarray(item)
+        if len(data.shape) > 1:
+            assert data.size == math.prod(data.shape), data.shape
+            data.resize(data.size)
+        return data
+
+    def __getitem__(self, index: int) -> numpy.ndarray:
+        """Return the nubar data stored at ``time[index]``"""
+        return self._nubar[index]
+
+    def __setitem__(self, index: int, nubar: numpy.ndarray):
+        """Overwrite nubar data stored at index ``index``"""
+        self._nubar[index] = self._validate(nubar)
+        self._coeffs = None
+
+    def _insert(self, index: int, value: numpy.ndarray):
+        self._nubar.insert(index, self._validate(value))
+        self._coeffs = None
+
+    def _append(self, value: numpy.ndarray):
+        self._nubar.append(self._validate(value))
+        self._coeffs = None
+
+    def _extend(self, values):
+        valid = [self._validate(v) for v in values]
+        self._nubar.extend(valid)
+
+    def __call__(self, time: float) -> numpy.ndarray:
+        """Return nubar across burnable regions at a specific time
+
+        Apply polynomial fitting using previous number of nubar values
+
+        Parameters
+        ----------
+        time : float
+            Point in calendar time to evaluate
+
+        Returns
+        -------
+        numpy.ndarray
+            Homogenized "macroscopic" nu-bar data in each burnable region
+
+        """
+        return super().__call__(time)
+
+    def _evaluate(self, time: float) -> numpy.ndarray:
+        if self._coeffs is None:
+            if len(self) <= self._order:
+                raise AttributeError(
+                    "Need more than {order} items for {order} nubar fitting. "
+                    "Have {n}".format(order=self._order, n=len(self))
+                )
+            self._coeffs = polyfit(self._times, numpy.array(self._nubar), self._order)
+        return polyval(time, self._coeffs)
