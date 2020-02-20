3d9a57ca4336781d4564fed6f755c905eb822145
Andrew Johnson <ajohnson400@gatech.edu>
2020-02-20 10:04:56 -0500

    Parametrize xs extrapolation test
    Ensure that the maximum allowed order will be used,
    up to the order requested by the user. If two points are given,
    constant and linear extrapolation can be done easily, but anything
    higher is not optimal nor possible. Numpy allows this, but will raise
    some warnings.
    
    The test is parametrized against fitting order, and the expected
    vector will be determined in the test given the fitting order.

--- tests/test_timemachine.py
+++ tests/test_timemachine.py
@@ -14,12 +14,30 @@ def microxs():
     )
 
 
-def test_oneGroupReactionRates(microxs):
+@pytest.mark.parametrize("order", (0, 1, 2))
+def test_oneGroupReactionRates(microxs, order):
+    """Test the validity of the microxs extrapolation
+
+    It is not impossible that a time machine will be created
+    using a single point in time, yet a linear or higher-order
+    fitting scheme is requested. This is not mathematically optimal
+    and may be unstable, but could be common for initial phases in
+    the sequence.
+
+    Rather than make an error, the process is to use the
+    highest available order is used. If a single point is given, yet
+    linear fitting is requested, constant fitting will be used until
+    two or more points are added to the collection.
+
+    This test is parametrized to study that behavior.
+
+    """
     times = [0, 100]
+    targetTime = 50
     incomingMicroXs = [[microxs], [microxs * 2]]
     expectedXsVector = microxs * 1.5
 
-    timemachine = hydep.internal.XsTimeMachine(1, times, incomingMicroXs)
+    timemachine = hydep.internal.XsTimeMachine(order, times, incomingMicroXs)
 
     flux = numpy.array([[4e6]])
 
@@ -29,9 +47,12 @@ def test_oneGroupReactionRates(microxs):
     # Quantities should be a single vector
     expectedRates = expectedXsVector.mxs[:, 0] * flux[0, 0]
 
-    rxns = timemachine.getReactionRatesAt(0.5 * (times[0] + times[1]), flux)
+    with pytest.warns(None) as record:
+        rxns = timemachine.getReactionRatesAt(targetTime, flux)
 
+    assert len(record) == 0
     assert len(rxns) == 1
     assert rxns[0].zai == expectedXsVector.zai
     assert rxns[0].rxns == expectedXsVector.rxns
     assert rxns[0].mxs == pytest.approx(expectedRates)
+
