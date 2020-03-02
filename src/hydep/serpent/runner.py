51221bf3ce6c8d8f85fb3c2f908b98c432ddfbfe
Andrew Johnson <ajohnson400@gatech.edu>
2020-03-02 16:44:06 -0500

    Provide ExtDepRunner (patched) Serpent runner
    Controls Serpent via signal-based coupling. Expects to use
    a patched version of Serpent that supports passing new compositions
    before every transport step, except the first step. Has some helpful
    methods for starting, stepping, and terminating the interface.

--- src/hydep/serpent/runner.py
+++ src/hydep/serpent/runner.py
@@ -1,9 +1,13 @@
 """Serpent runner"""
 
+import pathlib
+import time
 import logging
 import subprocess
 import numbers
 import os
+from enum import Enum, auto
+import signal
 
 from hydep import FailedSolverError
 
@@ -41,6 +45,7 @@ class BaseRunner:
         Serpent will be run using ``mpirun -np <N>``
 
     """
+
     def __init__(self, executable=None, omp=None, mpi=None):
         self.executable = executable
         self.omp = omp
@@ -218,4 +223,126 @@ class SerpentRunner(BaseRunner):
             raise FailedSolverError(msg)
 
 
+class STATE(Enum):
+    """Enumerations of various states for the external coupling"""
+    INACTIVE = auto()  #: State prior to starting the coupling
+    RUNNING = auto()  #: Serpent is currently running
+    WAIT_IFC = auto()  #: Transport has completed, waiting for interfaces
+    WAIT_NEXT_STEP = auto()  #: Waiting before next depletion step
+    TERM = auto()  #: Serpent has signaled that it is terminating
+    DEAD = auto()  #: Process has closed, possibly because it died
 
+
+class ExtDepRunner(BaseRunner):
+    """Class for running Serpent via the external depletion interface"""
+    def __init__(self, executable=None, omp=None, mpi=None):
+        super().__init__(executable, omp, mpi)
+        self._proc = None
+        self._state = STATE.INACTIVE
+        self._output = None
+
+    @property
+    def state(self):
+        return self._state
+
+    def _signalHandler(self, insig, stack):
+        if insig == signal.SIGUSR1:
+            self._state = STATE.WAIT_IFC
+        elif insig == signal.SIGUSR2:
+            self._state = STATE.WAIT_NEXT_STEP
+        elif insig == signal.SIGTERM:
+            self._state = STATE.TERM
+
+    def __del__(self):
+        if self._proc is not None:
+            self._proc.terminate()
+        if self._output is not None:
+            self._output.close()
+
+    def start(self, inputfile, output=None):
+        """Start coupled Serpent and run the first transport step
+
+        Parameters
+        ----------
+        inputfile : str or pathlib.Path
+            Path to Serpent input file
+        output : str or pathlib.Path or writable, optional
+            Destination for the output stream. If a string of path given,
+            write all outputs to a file with that name. If ``None``,
+            capture output internally. Otherwise, the object must
+            be writable, as all output from Serpent will be forwarded
+            to this location
+
+        """
+        cmd = self.makeCommand() + [inputfile]
+
+        if output is None:
+            output = subprocess.PIPE
+        elif isinstance(output, (str, pathlib.Path)):
+            output = self._output = open(output, "w")
+        else:
+            assert hasattr(output, "write")
+
+        signal.signal(signal.SIGUSR1, self._signalHandler)
+        signal.signal(signal.SIGUSR2, self._signalHandler)
+        signal.signal(signal.SIGTERM, self._signalHandler)
+
+        self._proc = subprocess.Popen(cmd, stdout=output, stderr=subprocess.STDOUT)
+        self._state = STATE.RUNNING
+        self._wait(STATE.WAIT_IFC)
+
+    def _wait(self, desiredState):
+        stat = self._proc.poll()
+        while stat is None:
+            if self.state == desiredState:
+                break
+            time.sleep(1)
+            stat = self._proc.poll()
+        else:
+            self._state = STATE.DEAD
+            # Avoid a race-condition where Serpent has told us it is
+            # terminating, and did so before we could check
+            # the state from the signal handler. If we wanted Serpent
+            # to terminate, then this should be okay
+            if desiredState == STATE.TERM:
+                return
+            # All is lost
+            raise subprocess.SubprocessError(
+                "Serpent has failed/terminated when it should not have. "
+                f"Exit code: {stat}"
+            )
+
+    def solveNext(self):
+        """Tell Serpent to solve the next transport step
+
+        Assumes that the compositions have already been written
+        according to the external depletion interface commands.
+
+        Serpent is first told to advance to the next point in
+        time. Serpent sends a signal that it is ready for the next
+        stage, and that any interfaces should be updated. In the
+        custom version of Serpent that presents this setting, the
+        depletion interface file is only written at the first stage.
+
+        Externally, through this framework, the compositions are
+        updated prior to calling this method. The second signal tells
+        Serpent the interfaces are ready and the next transport step
+        should be taken.
+
+        """
+        self._proc.send_signal(signal.SIGUSR2)
+        self._wait(STATE.WAIT_NEXT_STEP)
+
+        self._proc.send_signal(signal.SIGUSR1)
+        self._state = STATE.RUNNING
+        self._wait(STATE.WAIT_IFC)
+
+    def solveEOL(self):
+        """Advance to and solve the final point in time
+
+        Performs the same actions as :meth:`solveNext`, and then
+        waits for Serpent to terminate.
+        """
+        self.solveNext()
+        self._proc.send_signal(signal.SIGUSR2)
+        self._wait(STATE.TERM)
