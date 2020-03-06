f1fcfd4aea94339808f69ee530922e507cad20f6
Andrew Johnson <drewej@protonmail.com>
2020-03-05 21:37:47 -0500

    Add and test hydep.sfv.SfvSettings
    Responsible for handling number of modes, density cutoff,
    and a new value: mode fraction. Mode fraction indicates the fraction of
    available modes, bounded (0, 1], to use. Inherits from SubSetting,
    so it can be configured onto a HydepSettings instance with the
    {"hydep.sfv": {...}} nesting of values.

--- src/hydep/sfv/__init__.py
+++ src/hydep/sfv/__init__.py
@@ -1,1 +1,2 @@
+from .utils import SfvSettings
 from .solver import SfvSolver
---
+++ tests/test_sfv.py
@@ -0,0 +1,87 @@
+"""Unit tests for SFV interface, not module"""
+import pytest
+import hydep
+
+hsfv = pytest.importorskip("hydep.sfv")
+
+
+@pytest.mark.sfv
+def test_config():
+    """Test basic configuration, value checking, etc"""
+    settings = hsfv.SfvSettings(modes=1, modeFraction=0.5, densityCutoff=1e-6)
+
+    assert settings.modes == 1
+    assert settings.modeFraction == 0.5
+    assert settings.densityCutoff == 1e-6
+
+    with pytest.raises(ValueError):
+        settings.modeFraction = 0
+
+    with pytest.raises(ValueError):
+        settings.modeFraction = 50
+
+    assert settings.modeFraction == 0.5
+
+    with pytest.raises(TypeError):
+        settings.modes = 1.5
+
+    with pytest.raises(ValueError):
+        settings.modes = -1
+
+    with pytest.raises(ValueError):
+        settings.modes = 0
+
+    assert settings.modes == 1
+
+    settings.modes = None
+    assert settings.modes is None
+
+    with pytest.raises(ValueError):
+        settings.densityCutoff = -1
+
+    with pytest.raises(TypeError):
+        settings.densityCutoff = None
+
+    # Test user-friendly config
+    settings.update(
+        {"modes": "none", "mode fraction": "0.75", "density cutoff": "1E-2"}
+    )
+
+    assert settings.modes is None
+    assert settings.modeFraction == 0.75
+    assert settings.densityCutoff == 1e-2
+
+    with pytest.raises(TypeError):
+        settings.update({"modes": "1.7"})
+
+    with pytest.raises(ValueError):
+        settings.update({"mode fraction": "2.0"})
+
+    with pytest.raises(ValueError):
+        settings.update({"density cutoff": "-2"})
+
+    settings.update({"modes": "200"})
+    assert settings.modes == 200
+
+    with pytest.raises(ValueError):
+        settings.update({"modes": "200", "fake setting": "1"})
+
+
+
+def test_fromSettings():
+    """Test the integration into the dynamic settings framework"""
+    hsettings = hydep.settings.HydepSettings()
+
+    hsettings.updateAll(
+        {
+            "hydep": {},
+            "hydep.sfv": {
+                "modes": "1E6",
+                "mode fraction": "0.5",
+                "density cutoff": "1E-5",
+            },
+        }
+    )
+    assert hsettings.sfv.modes == 1E6
+    assert hsettings.sfv.modeFraction == 0.5
+    assert hsettings.sfv.densityCutoff == 1E-5
